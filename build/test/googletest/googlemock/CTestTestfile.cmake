0c7ab3bf738da8bab11fb70afd862853f4647da5
amach8 <98847891+amach8@users.noreply.github.com>
2024-05-27 00:35:17 -0700

    lol

--- build/test/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aaronnmsch/Downloads/TaskPro/test/googletest/googlemock
-# Build directory: /Users/aaronnmsch/Downloads/TaskPro/build/test/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/test/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/aaronnmsch/Downloads/TaskPro/test/googletest/googletest
-# Build directory: /Users/aaronnmsch/Downloads/TaskPro/build/test/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
